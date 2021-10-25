4f9d43680613aca77d4df208629d4685d4cf0009
Loic <90761328+LoicSelency@users.noreply.github.com>
2021-10-25 16:41:43 +0200

    Update selency.js

--- _data/sites/selency.js
+++ _data/sites/selency.js
@@ -1,12 +1,16 @@
 module.exports = {
 	name: "Selency", // optional, falls back to object key
-	description: "The default sites that get tested",
+	description: "Selency audits",
 	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
+		frequency: !process.env.CONTEXT ? 10 : 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
 		"https://www.selency.fr/",
 		"https://www.selency.fr/meubles.html",
-		"https://www.selency.fr/produit/PV4JH5U1/commode-scandinave-vintage.html"
+		"https://www.selency.fr/nouveaux-produits.html",
+		"https://www.selency.fr/produit/DVFS9DT5/piero-fornasetti-boite-en-acajou-papillons-milan-italie-vers-1950.html",
+		"https://www.selency.fr/selections/design",
+		"https://www.selency.fr/designers/aalto-alvar.html",
+		"https://www.selency.fr/page/nos-engagements"
 	]
 };
