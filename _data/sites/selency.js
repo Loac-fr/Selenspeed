14be06c1d5ca0734d0b7e2f0f36164047c8d3989
Loic <90761328+LoicSelency@users.noreply.github.com>
2021-12-20 11:35:57 +0100

    Update selency.js

--- _data/sites/selency.js
+++ _data/sites/selency.js
@@ -8,7 +8,8 @@ module.exports = {
 		"https://www.selency.fr/",
 		"https://www.selency.fr/meubles.html",
 		"https://www.selency.fr/nouveaux-produits.html",
-		"https://www.selency.fr/produit/DVFS9DT5/piero-fornasetti-boite-en-acajou-papillons-milan-italie-vers-1950.html",
+		"https://www.selency.fr/produit/87HPRFFB/commode-style-regence.html",
+		"https://www.selency.fr/produit/XVWGZ7ZZ/plat-carre-faience-de-gien-decor-pivoine.html",
 		"https://www.selency.fr/selections/design",
 		"https://www.selency.fr/designers/aalto-alvar.html",
 		"https://www.selency.fr/page/nos-engagements"
